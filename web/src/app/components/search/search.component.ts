dbfb631071cafcf1f65c27735387350fa7e07ffd
Rishabh Jain <rishabhjainnsit@gmail.com>
2018-07-01 11:12:22 +0530

    working dropdowns without bootstrap

--- web/src/app/components/search/search.component.ts
+++ web/src/app/components/search/search.component.ts
@@ -8,23 +8,43 @@ import { EnumsService } from '../../services/enums.service';
 })
 export class SearchComponent implements OnInit {
 
+  public interviewTypes;
+  public countries;
+  public allVisaTypes;
   public visaTypes;
   public consulates;
+  public selectedCountry;
+  public selectedVisaType;
   constructor(private enumsService: EnumsService) { }
 
   ngOnInit() {
+    this.interviewTypes = ["Visa", "Job"];
+    this.countries = ["USA", "UK"];
+    this.visaTypes = [];
     this.getVisaTypes();
     this.getConsulates();
   }
 
+  selectCountryChangeHandler (event: any) {
+    this.selectedCountry = event.target.value;
+    
+    var allVisaTypesLocal = [];
+    this.allVisaTypes.forEach(function(visaType){
+     if(visaType.country == event.target.value) {
+      allVisaTypesLocal.push(visaType);
+     }
+  });
+  this.visaTypes = allVisaTypesLocal;
+    console.log("selected " + this.selectedCountry);
+  }
 
   getVisaTypes() {
     this.enumsService.getVisaTypes().subscribe( 
-      data => {this.visaTypes = data},
+      data => {this.allVisaTypes = data},
       err => console.error(err),
-      () => console.log("Fetched "+ this.visaTypes.length +" visaTypes")
+      () => console.log("Fetched "+ this.allVisaTypes.length +" visaTypes")
     );
-  }
+  } 
 
   getConsulates() {
     this.enumsService.getConsulates().subscribe( 
